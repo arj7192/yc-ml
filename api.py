12d8def100343d9ecf03b3a18bf863303f35c0f9
ashishranjanjha <ashishranjanjha@tractable.ai>
2019-05-19 14:41:16 +0100

    sentiment analysis

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ from flask import Flask, request, Response, send_file
 from yc import *
 from word_count import *
 from topic_modelling import *
+from sentiment_analysis import *
 
 app = Flask(__name__)
 
@@ -33,6 +34,19 @@ def get_tm_api(video_id):
         get_comments(youtube, thread["id"], comments)
 
     return get_topics_ngram(comments, ngram=ngram)
+
+
+@app.route('/comments/sa/<video_id>', methods=['GET'], )
+def get_sa_api(video_id):
+    """
+    """
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+    comments = []
+    video_comment_threads = get_comment_threads(youtube, video_id, comments)
+    for thread in video_comment_threads:
+        get_comments(youtube, thread["id"], comments)
+
+    return get_sentiment_analysis(comments)
 
 
 if __name__ == '__main__':
